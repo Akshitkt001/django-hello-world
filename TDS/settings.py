176bfc5acb7b8c263024a186aa1ee1846c2fbde5
Akshit Kumar Tiwari <137613929+Akshitkt001@users.noreply.github.com>
2024-08-10 15:02:00 +0530

    Update settings.py

--- TDS/settings.py
+++ TDS/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k0+kxvhynem@4_u5yi0gd@4d9%h0kjsn1e=r+p90sjx@*mer0y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
